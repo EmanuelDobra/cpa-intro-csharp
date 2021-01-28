eaeb6e60f2877a387ae57c6b3b523591a238972b
Emanuel Dobra <ema.dobra@gmail.com>
2021-01-28 17:19:35 -0500

    Added Lab01

---
+++ Assignments/Lab01.cs
@@ -0,0 +1,83 @@
+using System;
+
+/* Emanuel Dobra
+   Lab 1         */
+
+namespace Lab1
+{
+    class Program
+    {
+        const double TRIP_DISTANCE= 500.0;
+
+        static void showTripDistance()
+        {
+            Console.WriteLine("Trip length: " + TRIP_DISTANCE);
+        }
+
+        static int chooseCar()
+        {
+            int userChoice;
+            Console.WriteLine("Please enter the number that represents your vehicle?" +
+                " 1=Honda Civic, 2 =Toyota Camry, 3 =Ford F - 150");
+            userChoice = Int32.Parse(Console.ReadLine());
+            return userChoice;
+        }
+
+        static int checkCarSpeed(int carType)
+        {
+            switch(carType)
+            {
+                case 1:
+                    return 10;
+                case 2:
+                    return 20;
+                case 3:
+                    return 40;
+                default:
+                    return 0;
+            }
+        }
+
+        static double calcTripTime(int carSpeed)
+        {
+            return (TRIP_DISTANCE/carSpeed);
+        }
+
+        static void Main(string[] args)
+        {
+            int car, carSpeed;
+            double tripTime = 0;
+            for (int loop = 0; loop < 3; loop++)
+            {
+                showTripDistance();
+                car = chooseCar();
+                carSpeed = checkCarSpeed(car);
+                tripTime = calcTripTime(carSpeed);
+                Console.WriteLine("Total time taken for the trip: " + tripTime + "\n");
+            }
+            // Check last value of total trip time
+            Console.WriteLine("Total time scope: " + tripTime);
+            Console.ReadLine(); // Stop console from closing
+        }
+
+    }
+}
+/**
+Output:
+Trip length: 500
+Please enter the number that represents your vehicle? 1=Honda Civic, 2 =Toyota Camry, 3 =Ford F - 150
+1
+Total time taken for the trip: 50
+
+Trip length: 500
+Please enter the number that represents your vehicle? 1=Honda Civic, 2 =Toyota Camry, 3 =Ford F - 150
+2
+Total time taken for the trip: 25
+
+Trip length: 500
+Please enter the number that represents your vehicle? 1=Honda Civic, 2 =Toyota Camry, 3 =Ford F - 150
+3
+Total time taken for the trip: 12.5
+
+Total time scope: 12.5
+*/
